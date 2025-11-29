d6a68d587d97a13779c48a13295bdd062b0d9c5a
MD Imran <138973820+prodeveloper09@users.noreply.github.com>
2025-11-29 20:25:52 +0600

    es6 tamplate

---
+++ tamplate.js
@@ -0,0 +1,14 @@
+const fname = "Justin";
+const lname = "TimberLake";
+
+const fullName = fname + " " +lname;
+
+const fullName2 = `${fname} ${lname} ${20+80+90} is a good boy`;
+
+const multiLine = "I love you\n" + "I miss you\n" + 
+"I djjd sdye";
+
+const multiLine2 = `I love you
+I miss you
+no. I don't need you`
+console.log(multiLine2);
